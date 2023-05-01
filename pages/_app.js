4e2113c523983d1998e2df1c7359bdcdd4646b72
sajad <sajadice2@gmail.com>
2023-05-01 17:52:13 +0330

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,48 @@
-import '@/styles/globals.css'
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//MUi Themes
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "../themes/index";
+
+//
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+
+//Cache provide MUI
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import createEmotionCache from "../src/createEmotionCache";
+
+//Global css
+import "../styles/globals.css";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  // Create rtl cache
+  const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+  return (
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
